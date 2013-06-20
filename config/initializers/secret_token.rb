9db5a408e4b90077a391b5b1470aa5a02e01930b
Jason LeMonier <jlemonier@gmail.com>
2013-06-20 07:39:44 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dragonnav::Application.config.secret_token = '1510ecc36793d853d6325039f388bae7fd86caeebec628097bb53952b79798cf548abdabbd4f541a780df3107843fccbee9b0e3abc7d0b45744a03932263f4b2'
